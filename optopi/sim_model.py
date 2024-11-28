4dff9ee1e3ad101b0d1b5b6e123a6f65972c5dcc
Phuong Ho <phuongho43@gmail.com>
2024-11-28 12:50:08 -0800

    Add sparse channel simulation code

--- optopi/sim_model.py
+++ optopi/sim_model.py
@@ -7,7 +7,7 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 
-from optopi.model import sim_lid, sim_lov
+from optopi.model import sim_lid, sim_lov, sim_sparser
 
 CUSTOM_STYLE = {
     "image.cmap": "turbo",
@@ -97,48 +97,74 @@ def plot_model_fit(fig_fp, k_json_fp, y_csv_fp, u_csv_fp, ode_model):
     plt.close("all")
 
 
-# def plot_figure_s10a():
-#     # Model for sparse channel
-#     # LOVfast parameters
-#     kl1 = 51.982
-#     kd1 = 0.369
-#     kb1 = 3.407
-#     # iLIDslow parameters
-#     kl2 = 14.997
-#     kd2 = 0.008
-#     kb2 = 39.708
-#     data_dp = "/home/phuong/protosignet/1--biosensor/4--validation/0--sparse_ch/results/"
-#     data_ucsv_fp = os.path.join(data_dp, "u.csv")
-#     data_ycsv_fp = os.path.join(data_dp, "y.csv")
-#     u_df = pd.read_csv(data_ucsv_fp)
-#     y_df = pd.read_csv(data_ycsv_fp)
-#     u_df = u_df.groupby("t", as_index=False)["u"].mean()
-#     y_df = y_df.groupby("t", as_index=False)["y"].mean()
-#     tu_data = u_df["t"].values
-#     uu_data = u_df["u"].values
-#     ty_data = y_df["t"].values
-#     yy_data = y_df["y"].values
-#     yy_data = yy_data - np.min(yy_data)
-#     y0_data = [0, 0, 0, 0, np.max(yy_data), np.max(yy_data), 0, 0, yy_data[0]]
-#     yd_df = pd.DataFrame({"t": ty_data, "y": yy_data})
-#     kk = [kl1, kd1, kb1, kl2, kd2, kb2]
-#     tm, Xm = sim_sparser(tu_data, y0_data, uu_data, kk)
-#     yy_model = Xm[-1]
-#     ym_df = pd.DataFrame({"t": tm, "y": yy_model})
-#     fig_fp = os.path.join("/home/phuong/protosignet/3--figures/", "fig_s10a.png")
-#     group_labels = ["Sparse Ch.\n(ddFP)", "Sparse Ch.\n(model)"]
-#     plot_uy(
-#         fig_fp=fig_fp,
-#         y_df=ym_df,
-#         u_df=u_df,
-#         z_df=yd_df,
-#         xlabel="Time (s)",
-#         ylabel=r"$\mathbf{AU}$",
-#         ulabel="BL",
-#         group_labels=group_labels,
-#         lgd_loc="lower right",
-#         dpi=100,
-#     )
+def sim_sparser(fig_fp, k_lovfast_json_fp, k_lidslow_json_fp, y_csv_fp, u_csv_fp):
+    fig_fp_pl = Path(fig_fp)
+    fig_fp_pl.parent.mkdir(parents=True, exist_ok=True)
+    with open(k_lovfast_json_fp) as jf:
+        kk_lovfast = json.load(jf)
+    with open(k_lidslow_json_fp) as jf:
+        kk_lidslow = json.load(jf)
+    kk = {
+        "kl1": kk_lovfast["kl"],
+        "kd1": kk_lovfast["kd"],
+        "kb1": kk_lovfast["kb"],
+        "kl2": kk_lidslow["kl"],
+        "kd2": kk_lidslow["kd"],
+        "kb2": kk_lidslow["kb"],
+    }
+    yd_df = pd.read_csv(y_csv_fp)
+    y_df = yd_df.groupby("t", as_index=False)["y"].agg(["mean"])
+    yy = y_df["mean"].to_numpy()
+    ud_df = pd.read_csv(u_csv_fp)
+    u_df = ud_df.groupby("t", as_index=False)["u"].agg(["mean"])
+    tu = u_df["t"].to_numpy()
+    uu = u_df["mean"].to_numpy()
+    yd_df["y"] = yd_df["y"] - np.min(yy)
+    yy = yy - np.min(yy)
+    y0 = [0, 0, 0, 0, np.max(yy), np.max(yy), 0, 0, yy[0]]
+    tm, ym = ode_model(tu, y0, uu, kk)
+    ym_df = pd.DataFrame({"t": tm, "y": ym[-1]})
+    with plt.style.context(("seaborn-v0_8-whitegrid", CUSTOM_STYLE)):
+        fig, ax = plt.subplots(figsize=(24, 20))
+        sns.lineplot(
+            data=yd_df,
+            x="t",
+            y="y",
+            ax=ax,
+            estimator="mean",
+            errorbar=("se", 1.96),
+            linewidth=0,
+            color="#34495E",
+            marker="o",
+            markersize=12,
+            zorder=5,
+        )
+        sns.lineplot(data=ym_df, x="t", y="y", ax=ax, color="#648FFF", zorder=4)
+        handles = [
+            mpl.lines.Line2D([], [], color="#34495E", marker="o", markersize=8, linewidth=0),
+            mpl.lines.Line2D([], [], color="#648FFF", linewidth=16),
+        ]
+        group_labels = ["Data", "Model"]
+        ax.legend(
+            handles,
+            group_labels,
+            loc="best",
+            markerscale=4,
+            frameon=True,
+            shadow=False,
+            handletextpad=0.4,
+            borderpad=0.2,
+            labelspacing=0.2,
+            handlelength=1,
+        )
+        ax.set_xlabel("Time (s)")
+        ax.set_ylabel("AU")
+        ax.locator_params(axis="x", nbins=10)
+        ax.locator_params(axis="y", nbins=10)
+        fig.tight_layout()
+        fig.canvas.draw()
+        fig.savefig(fig_fp, pad_inches=0.3, dpi=100, bbox_inches="tight", transparent=False)
+    plt.close("all")
 
 
 # def plot_figure_s10c():
@@ -229,12 +255,26 @@ def plot_model_fit(fig_fp, k_json_fp, y_csv_fp, u_csv_fp, ode_model):
 
 
 def main():
-    fig_fp = "/home/phuong/projects/optopi/example/sim_model/LOV/I427V/model-fit.png"
-    k_json_fp = "/home/phuong/projects/optopi/example/fit_model/LOV/I427V/fit_params.json"
-    y_csv_fp = "/home/phuong/projects/optopi/example/data/LOV/I427V/y.csv"
-    u_csv_fp = "/home/phuong/projects/optopi/example/data/LOV/I427V/u.csv"
-    ode_model = sim_lov
-    plot_model_fit(fig_fp, k_json_fp, y_csv_fp, u_csv_fp, ode_model)
+    optopi_root_dp = Path(__file__).resolve().parent
+    example_dp = optopi_root_dp / "example"
+
+    # Plot comparison of model fit and corresponding data
+    for prot in ["LOV", "LID"]:
+        for mut in ["I427V", "V416I"]:
+            fig_fp = example_dp / "sim_model" / prot / mut / "model-fit.png"
+            k_json_fp = example_dp / "fit_model" / prot / mut / "fit_params.json"
+            y_csv_fp = example_dp / "data" / prot / mut / "y.csv"
+            u_csv_fp = example_dp / "data" / prot / mut / "u.csv"
+            ode_model = sim_lov if prot == "LOV" else sim_lid
+            plot_model_fit(fig_fp, k_json_fp, y_csv_fp, u_csv_fp, ode_model)
+
+    # Plot Sparse Decoder model prediction compared to corresponding data
+    fig_fp = example_dp / "sim_model" / "sparse_decoder" / "prediction.png"
+    k_lovfast_json_fp = example_dp / "fit_model" / "LOV" / "I427V" / "fit_params.json"
+    k_lidslow_json_fp = example_dp / "fit_model" / "LID" / "V416I" / "fit_params.json"
+    y_csv_fp = example_dp / "data" / "sparse_decoder" / "y.csv"
+    u_csv_fp = example_dp / "data" / "sparse_decoder" / "u.csv"
+    sim_sparser(fig_fp, k_lovfast_json_fp, k_lidslow_json_fp, y_csv_fp, u_csv_fp)
 
 
 if __name__ == "__main__":
